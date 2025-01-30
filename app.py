145bd5d18fd6c8978877c5701bd861139606c2f4
balaji-29 <56034559+balaji-29@users.noreply.github.com>
2025-01-30 12:15:09 +0530

    Update app.py
    for ssl ceritificate issue added certifi package

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from routes import pages
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+import certifi
 
 load_dotenv()
 
 def create_app():
   app=Flask(__name__)
-  client = MongoClient(os.environ.get("MONGODB_URI"))
+  client = MongoClient(os.environ.get("MONGODB_URI"), tlsCAFile=certifi.where())
   app.db = client.get_default_database()
   
   app.register_blueprint(pages)
